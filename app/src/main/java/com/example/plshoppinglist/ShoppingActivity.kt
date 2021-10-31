02378d0d66ad8641ad1e0dd90c8172aa7a244fdf
Brian Donnoe <dunow33@hotmail.com>
2021-10-31 17:44:06 -0400

    finished video 3

--- app/src/main/java/com/example/plshoppinglist/ShoppingActivity.kt
+++ app/src/main/java/com/example/plshoppinglist/ShoppingActivity.kt
@@ -4,12 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import com.example.plshoppinglist.dao.ShoppingDatabase
+import com.example.plshoppinglist.dao.repositories.ShoppingRepository
 import com.example.plshoppinglist.ui.shoppinglist.ShoppingViewModel
+import com.example.plshoppinglist.ui.shoppinglist.ShoppingViewModelFactory
 
 class ShoppingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shopping)
+
+        val database = ShoppingDatabase(this)
+
+        val repository = ShoppingRepository(database)
+        val factory = ShoppingViewModelFactory(repository)
 
         val viewModel = ViewModelProviders.of(this).get(ShoppingViewModel::class.java)
     }
--- app/src/main/java/com/example/plshoppinglist/ui/shoppinglist/ShoppingViewModelFactory.kt
+++ app/src/main/java/com/example/plshoppinglist/ui/shoppinglist/ShoppingViewModelFactory.kt
@@ -1,4 +1,15 @@
 package com.example.plshoppinglist.ui.shoppinglist
 
-class ShoppingViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.plshoppinglist.dao.repositories.ShoppingRepository
+
+@Suppress("UNCHECKED_CAST")
+class ShoppingViewModelFactory(
+    private val repository: ShoppingRepository
+): ViewModelProvider.NewInstanceFactory() {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return ShoppingViewModel(repository) as T
+    }
 }
