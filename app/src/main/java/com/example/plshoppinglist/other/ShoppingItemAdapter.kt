075db675e24bb10c748489b80af7988473c8c15e
Brian Donnoe <dunow33@hotmail.com>
2021-10-31 18:19:34 -0400

    finished video 4

---
+++ app/src/main/java/com/example/plshoppinglist/other/ShoppingItemAdapter.kt
@@ -0,0 +1,53 @@
+package com.example.plshoppinglist.other
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.plshoppinglist.R
+import com.example.plshoppinglist.dao.db.entities.ShoppingItem
+import com.example.plshoppinglist.ui.shoppinglist.ShoppingViewModel
+import kotlinx.android.synthetic.main.shopping_item.view.*
+
+class ShoppingItemAdapter(
+    var items: List<ShoppingItem>,
+    private val viewModel: ShoppingViewModel
+): RecyclerView.Adapter<ShoppingItemAdapter.ShoppingViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShoppingViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.shopping_item,
+            parent,
+            false)
+        return ShoppingViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ShoppingViewHolder, position: Int) {
+        val curShoppingItem = items[position]
+
+        holder.itemView.tvName.text = curShoppingItem.name
+        holder.itemView.tvAmount.text = "${curShoppingItem.name}"
+
+        holder.itemView.ivDelete.setOnClickListener {
+            viewModel.delete(curShoppingItem)
+        }
+
+        holder.itemView.ivPlus.setOnClickListener {
+            curShoppingItem.amount++
+            viewModel.upsert(curShoppingItem)
+        }
+
+        holder.itemView.ivMinus.setOnClickListener {
+            if(curShoppingItem.amount > 0) {
+                curShoppingItem.amount--
+                viewModel.upsert(curShoppingItem)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    class ShoppingViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+}
